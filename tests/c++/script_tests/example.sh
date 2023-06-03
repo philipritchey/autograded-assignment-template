bd6d1256faedd0ac74f62c562612ab5f6f070b00
Philip Ritchey <philipritchey@users.noreply.github.com>
2023-06-03 18:32:20 -0500

    Update example.sh

--- tests/c++/script_tests/example.sh
+++ tests/c++/script_tests/example.sh
@@ -2,6 +2,23 @@
 
 # an example script (custom) test
 
+usage() {
+  echo "Usage: $0 [-h]"
+  echo "  -h show this help message and exit"
+}
+
+debugmode=0
+while getopts "dht:" flag; do
+  case "${flag}" in
+    h | *) # display help
+      usage >> DEBUG
+      echo 100 > OUTPUT
+      echo "PASS: correct behavior" >> DEBUG
+      exit 0
+      ;;
+  esac
+done
+
 if g++ -std=c++17 -pedantic-errors -g code.cpp code_interactive.cpp > DEBUG 2>&1; then
     echo "X" | ./a.out > OUT
     if grep -qi "invalid input" OUT; then
