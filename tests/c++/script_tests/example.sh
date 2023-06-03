1a69159c2864355b643b50f40ca9d59179cf1bae
Philip Ritchey <philipritchey@users.noreply.github.com>
2023-06-03 18:26:36 -0500

    Create example.sh

---
+++ tests/c++/script_tests/example.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+
+# an example script (custom) test
+
+if g++ -std=c++17 -pedantic-errors -g code.cpp code_interactive.cpp > DEBUG 2>&1; then
+    echo "X" | ./a.out > OUT
+    if grep -qi "invalid input" OUT; then
+        echo 100 > OUTPUT
+        echo "PASS: correct behavior" >> DEBUG
+    else
+        echo 0 > OUTPUT
+        echo "FAIL: incorrect behavior" >> DEBUG
+    fi
+else
+    echo 0 > OUTPUT
+    echo "FAIL: compilation errors" >> DEBUG
+fi
