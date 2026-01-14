f10bbcb21c5b6fb2ab2a4207f6b57c6a59889573
philip ritchey <pcr@tamu.edu>
2026-01-14 08:31:41 -0600

    remove python version check because ubuntu 22 has python3.10 by default.

--- setup.sh
+++ setup.sh
@@ -7,17 +7,6 @@ apt-get -y upgrade
 # install dependencies
 apt-get -y install apt-utils valgrind num-utils
 
-# python version check (run_tests.py requires 3.8+)
-typeset -i python3_version=$(python3 --version | cut -d. -f2)
-if [ $python3_version -lt 8 ]; then
-  if [ "$(which python3.8)" == ""  ]; then
-    # install python3.8
-    apt-get install software-properties-common
-    add-apt-repository -y ppa:deadsnakes/ppa
-    apt-get -y install python3.8
-  fi
-fi
-
 cd /autograder/source
 mkdir -p /root/.ssh
 cp ssh_config /root/.ssh/config
