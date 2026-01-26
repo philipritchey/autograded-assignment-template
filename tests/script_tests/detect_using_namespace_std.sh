7a36fe09b321b75c025c9e83136f633f6ec69ee4
philip ritchey <pcr@tamu.edu>
2026-01-26 16:36:15 -0600

    add [FAIL] tag and message for passing

--- tests/script_tests/detect_using_namespace_std.sh
+++ tests/script_tests/detect_using_namespace_std.sh
@@ -1,8 +1,9 @@
 # find out if student used `using namespace std` anywhere
 # TODO(you): change target file(s)
 if grep -E "using\s+namespace\s+std" code.cpp; then
-  echo "Use of \`using namespace std\` is considered unsafe. 1-point penalty applied." >> DEBUG
+  echo "[FAIL] Use of \`using namespace std\` is considered unsafe. 1-point penalty applied." >> DEBUG
   echo "-1" > OUTPUT
 else
+  echo "[PASS] \`using namespace std\` not found.  good." >> DEBUG
   echo "100" > OUTPUT
 fi
