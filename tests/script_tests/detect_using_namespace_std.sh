69bda92f22648739ad745e53b19675baf497f1f7
philip ritchey <pcr@tamu.edu>
2026-02-08 16:26:03 -0600

    add support for multiple files to check for using namespace std

--- tests/script_tests/detect_using_namespace_std.sh
+++ tests/script_tests/detect_using_namespace_std.sh
@@ -1,9 +1,12 @@
 # find out if student used `using namespace std` anywhere
-# TODO(you): change target file(s)
-if grep -E "using\s+namespace\s+std" code.cpp; then
-  echo "[FAIL] Use of \`using namespace std\` is considered unsafe. 1-point penalty applied." >> DEBUG
-  echo "-1" > OUTPUT
-else
-  echo "[PASS] \`using namespace std\` not found.  good." >> DEBUG
-  echo "100" > OUTPUT
-fi
+# TODO(you): list files_to_check
+files_to_check=( code.cpp )
+for file in "${files_to_check[@]}"; do
+    if grep -E "using\s+namespace\s+std" $file; then
+      echo "[FAIL] Use of \`using namespace std\` is considered unsafe. 1-point penalty applied." >> DEBUG
+      echo "-1" > OUTPUT
+      exit 0
+    fi
+done
+echo "[PASS] \`using namespace std\` not found.  good." >> DEBUG
+echo "100" > OUTPUT
