4d06e7ec47db5a0a1f05b33c6abe63048f495b08
philip ritchey <pcr@tamu.edu>
2026-02-28 22:18:35 -0600

    add file name to output for detect using namespace std

--- tests/script_tests/detect_using_namespace_std.sh
+++ tests/script_tests/detect_using_namespace_std.sh
@@ -3,7 +3,7 @@
 files_to_check=( code.cpp )
 for file in "${files_to_check[@]}"; do
     if grep -E "using\s+namespace\s+std" $file; then
-      echo "[FAIL] Use of \`using namespace std\` is considered unsafe. 1-point penalty applied." >> DEBUG
+      echo "[FAIL] ${file}: Use of \`using namespace std\` is considered unsafe. 1-point penalty applied." >> DEBUG
       echo "-1" > OUTPUT
       exit 0
     fi
